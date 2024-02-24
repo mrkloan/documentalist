505fe22e93ef5a9372ffad3211ea8b5c6d8844a3
Valentin Fries <valentin.fries@blablacar.com>
2024-02-24 23:15:39 +0100

    Define Ollama base URL as env variable

--- documentalist/__main__.py
+++ documentalist/__main__.py
@@ -3,7 +3,7 @@ from langchain_community.llms import Ollama
 
 
 def main():
-    llm = Ollama(model=os.getenv('OLLAMA_MODEL'))
+    llm = Ollama(base_url=os.getenv('OLLAMA_BASE_URL'), model=os.getenv('OLLAMA_MODEL'))
     print(llm.invoke("Tell me a joke"))
 
 

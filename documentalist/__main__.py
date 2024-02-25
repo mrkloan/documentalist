cbaf14249066098cce343574ff5b185858ddf787
Valentin Fries <valentin.fries@blablacar.com>
2024-02-25 18:47:34 +0100

    Proper CLI application

--- documentalist/__main__.py
+++ documentalist/__main__.py
@@ -1,3 +1,4 @@
+import click
 import os
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_community.document_loaders import DirectoryLoader
@@ -9,77 +10,21 @@ from langchain_core.output_parsers import StrOutputParser
 from langchain_core.prompts import PromptTemplate
 from langchain_core.runnables import RunnablePassthrough
 
-
-def format_docs(docs):
-    return "\n\n".join(doc.page_content for doc in docs)
-
-
-def main():
-    print("Instantiating embeddings...")
-    embeddings = OllamaEmbeddings(
-        base_url=os.getenv('OLLAMA_BASE_URL'),
-        model=os.getenv('OLLAMA_MODEL')
-    )
-    print("Instantiating the vectorstore...")
-    chromadb = Chroma(
-        persist_directory="./data/chromadb",
-        embedding_function=embeddings,
-        collection_name="documentalist"
-    )
-    print("Checking the vectorstore content...")
-    collection = chromadb.get()
-    print("\tNumber of documents in the vectorstore: {:d}".format(len(collection['documents'])))
-
-    if len(collection['ids']) == 0:
-        print("Instantiating document loader...")
-        loader = DirectoryLoader(
-            './data/dataset',
-            glob="**/*.pdf",
-            loader_cls=PyPDFLoader,
-            use_multithreading=True
-        )
-        print("Loading documents from the dataset...")
-        documents = loader.load()
-        print("\tNumber of loaded documents: {:d}".format(len(documents)))
-
-        print("Instantiating document splitter...")
-        splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=128)
-        print("Splitting documents...")
-        splits = splitter.split_documents(documents)
-        print("\tNumber of split documents: {:d}".format(len(splits)))
-
-        print("Initializing the vectorstore...")
-        chromadb = Chroma.from_documents(
-            documents=splits,
-            embedding=embeddings,
-            persist_directory="./data/chromadb",
-            collection_name="documentalist"
-        )
-        print("Persisting the vectorstore...")
-        chromadb.persist()
-
-    print('Instantiating the RAG chain...')
-    retriever = chromadb.as_retriever()
-    template = """\
-    You are an expert problem-solver, tasked with answering any question you might be asked.\
+PROMPT_TEMPLATE = """\
+    You are an expert problem-solver, tasked with answering any question you might be asked.
     
     Generate a comprehensive and informative answer of 80 words or less for the \
     given question based solely on the provided context (document name and content). \
     You must only use information from the provided context. Use an unbiased and \
     journalistic tone. Combine context results together into a coherent answer. Do not \
-    repeat text. Cite context documents using [${{number}}] notation. Only cite the most \
-    relevant contexts that answer the question accurately. Place these citations at the end \
-    of the sentence or paragraph that reference them - do not put them all at the end. If \
-    different results refer to different entities within the same name, write separate \
-    answers for each entity.
-    
-    You should use bullet points in your answer for readability. Put citations where they apply
-    rather than putting them all at the end.
+    repeat text. Cite the most relevant contexts that answer the question accurately. \
+    You should use bullet points in your answer for readability. Put citations where they apply \
+    rather than putting them all at the end. 
     
     If there is nothing in the context relevant to the question at hand, just say "Hmm, \
-    I'm not sure." Don't try to make up an answer.
+    I'm not sure." Don't try to make up an answer. \
     
-    Anything between the following `context`  html blocks is retrieved from a knowledge \
+    Anything between the following `context` html blocks is retrieved from a knowledge \
     bank, not part of the conversation with the user. 
     
     <context>
@@ -89,13 +34,93 @@ def main():
     REMEMBER: If there is no relevant information within the context, just say "Hmm, I'm \
     not sure." Don't try to make up an answer. Anything between the preceding 'context' \
     html blocks is retrieved from a knowledge bank, not part of the conversation with the \
-    user.\
+    user.
     
-    Question: {question}\
+    Question: {question}
     
     Helpful Answer:"""
-    rag_prompt = PromptTemplate.from_template(template)
-    llm = Ollama(base_url=os.getenv('OLLAMA_BASE_URL'), model=os.getenv('OLLAMA_MODEL'))
+
+
+@click.group()
+def cli():
+    """Train a custom RAG on your private PDF documents."""
+    pass
+
+
+@cli.command()
+@click.option('--base-url', default=os.getenv('OLLAMA_BASE_URL'), help='The base URL of the Ollama server.')
+@click.option('--model', default=os.getenv('OLLAMA_MODEL'), help='The name of the Ollama model to use.')
+@click.option('--chromadb-path', default=os.getenv('CHROMADB_PATH'), help='The path to the persisted ChromaDB data.')
+@click.option('--collection', default=os.getenv('CHROMADB_COLLECTION'), help='The newly trained collection.')
+@click.option('--dataset-path', default=os.getenv('DATASET_PATH'), help='The path to the training dataset.')
+def train(base_url, model, chromadb_path, collection, dataset_path):
+    """Train a custom RAG on your private PDF documents."""
+    click.echo("base_url: {:s}".format(base_url))
+    click.echo("model: {:s}".format(model))
+    click.echo("chromadb_path: {:s}".format(chromadb_path))
+    click.echo("collection: {:s}".format(collection))
+    click.echo("dataset_path: {:s}".format(dataset_path))
+
+    click.echo("[1/5] Loading documents from the dataset...")
+    loader = DirectoryLoader(
+        dataset_path,
+        glob="**/*.pdf",
+        loader_cls=PyPDFLoader,
+        use_multithreading=True
+    )
+    documents = loader.load()
+    click.echo("\tNumber of loaded documents: {:d}".format(len(documents)))
+
+    click.echo("[2/5] Splitting documents...")
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1024, chunk_overlap=128)
+    splits = splitter.split_documents(documents)
+    click.echo("\tNumber of split documents: {:d}".format(len(splits)))
+
+    click.echo("[3/5] Creating embeddings and initializing the vectorstore...")
+    embeddings = OllamaEmbeddings(
+        base_url=base_url,
+        model=model
+    )
+    chromadb = Chroma.from_documents(
+        documents=splits,
+        embedding=embeddings,
+        persist_directory=chromadb_path,
+        collection_name=collection
+    )
+
+    click.echo("[4/5] Persisting the vectorstore...")
+    chromadb.persist()
+
+    click.echo("[5/5] Done!")
+
+
+@cli.command()
+@click.option('--base-url', default=os.getenv('OLLAMA_BASE_URL'), help='The base URL of the Ollama server.')
+@click.option('--model', default=os.getenv('OLLAMA_MODEL'), help='The name of the Ollama model to use.')
+@click.option('--chromadb-path', default=os.getenv('CHROMADB_PATH'), help='The path to the persisted ChromaDB data.')
+@click.option('--collection', default=os.getenv('CHROMADB_COLLECTION'), help='The collection containing the RAG.')
+def chat(base_url, model, chromadb_path, collection):
+    """Chat with your custom LLM."""
+    click.echo("base_url: {:s}".format(base_url))
+    click.echo("model: {:s}".format(model))
+    click.echo("chromadb_path: {:s}".format(chromadb_path))
+    click.echo("collection: {:s}".format(collection))
+
+    embeddings = OllamaEmbeddings(
+        base_url=base_url,
+        model=model
+    )
+    chromadb = Chroma(
+        persist_directory=chromadb_path,
+        embedding_function=embeddings,
+        collection_name=collection
+    )
+    retriever = chromadb.as_retriever()
+    rag_prompt = PromptTemplate.from_template(PROMPT_TEMPLATE)
+    llm = Ollama(
+        base_url=base_url,
+        model=model
+    )
     rag_chain = (
             {"context": retriever | format_docs, "question": RunnablePassthrough()}
             | rag_prompt
@@ -103,12 +128,14 @@ def main():
             | StrOutputParser()
     )
 
-    print('Querying the LLM through the RAG chain...')
-    prompt = "What is the name of the routing algorithm introduced in the paper 'Intriguingly Simple and Fast Transit Routing'?"
+    prompt = click.prompt("Question")
     answer = rag_chain.invoke(prompt)
-    print("\tQuestion: {:s}".format(prompt))
-    print("\tAnswer: {:s}".format(answer.strip()))
+    click.echo(answer.strip())
+
+
+def format_docs(docs):
+    return "\n\n".join(doc.page_content for doc in docs)
 
 
 if __name__ == "__main__":
-    main()
+    cli()
